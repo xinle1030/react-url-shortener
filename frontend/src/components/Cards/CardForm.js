7f715d440fd8cc53656b670e6e32c19cc0682639
xinle1030 <xinle1030@gmail.com>
2022-11-19 17:07:17 +0800

    call API for url shortening when form is submitted

--- frontend/src/components/Cards/CardForm.js
+++ frontend/src/components/Cards/CardForm.js
@@ -1,8 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 // components
 
 export default function CardForm() {
+
+  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState("");
+
+  const onSubmit = (e) => {
+
+    e.preventDefault();
+
+    console.log(url);
+    console.log(title);
+
+    axios.defaults.baseURL = 'http://localhost:3333';
+    axios.post("/api/short", {
+      "origUrl": url,
+      "title": title
+    }).then(res =>
+      {
+        console.log(res.data);
+      }
+    ).catch(err => {
+      console.log(err.message);
+    })
+    setUrl("");
+  };
+
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
@@ -14,7 +40,7 @@ export default function CardForm() {
           </div>
         </div>
         <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <form>
+          <form onSubmit={onSubmit}>
             <h6 className="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
               Where you want to take your audience to?
             </h6>
@@ -30,7 +56,11 @@ export default function CardForm() {
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  required/>
+                  required
+                  placeholder="http://sampleurl.com"
+                  value={url}
+                  onChange={e => setUrl(e.target.value)}
+                  />
                 </div>
               </div>
 
@@ -45,6 +75,8 @@ export default function CardForm() {
                   <input
                     type="text"
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                    value={title}
+                  onChange={e => setTitle(e.target.value)}
                   />
                 </div>
               </div>
@@ -55,7 +87,7 @@ export default function CardForm() {
 
             <button
               className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-              type="button"
+              type="submit"
             >
               Create Your Url
             </button>
