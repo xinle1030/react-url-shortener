112aebede2d5f945b7d19ae89cf999fb2616fab6
xinle1030 <xinle1030@gmail.com>
2022-11-19 13:04:50 +0800

    update button label

--- frontend/src/components/Cards/CardForm.js
+++ frontend/src/components/Cards/CardForm.js
@@ -57,7 +57,7 @@ export default function CardForm() {
               className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
               type="button"
             >
-              Settings
+              Create Your Url
             </button>
           </form>
         </div>
