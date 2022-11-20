d362a81671394837dd39be2f6b9c207957be1749
xinle1030 <xinle1030@gmail.com>
2022-11-20 22:02:08 +0800

    format cardtable to display data per url

--- frontend/src/components/Cards/CardTable.js
+++ frontend/src/components/Cards/CardTable.js
@@ -1,31 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 // components
 
 export default function CardTable() {
-  const [visitInfo, setAllVisitInfo] = useState([]);
+  const [visitInfo, setVisitInfo] = useState([]);
+  const location = useLocation();
+  const urlId = location.state.urlId;
+
+  const getAllVisitInfo = async (visitInfoIds) => {
+    axios.defaults.baseURL = "http://localhost:3333";
+
+    axios
+      .get(
+        `/visitInfos?${visitInfoIds
+          .map((n, index) => `visitInfoIds[${index}]=${n}`)
+          .join("&")}`
+      )
+      .then((res) => {
+        setVisitInfo(JSON.stringify(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   function formatLinkTable(outputResult) {
-    if (outputResult !== "") {
-      const text = JSON.parse(outputResult);
-      console.log(text);
+    console.log(outputResult);
+    if (outputResult && outputResult.length > 0) {
+      const arr = JSON.parse(outputResult);
+
       const block = (
         <>
           <tbody>
-            {text.map((res, index) => (
-              <tr>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  {res.title ? res.title : "-"}
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  {res.title ? res.title : "-"}
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  {res.clicks}
-                </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  {res.title ? res.title : "-"}
-                </td>
+            {arr.map((res, index) => (
+              <tr key={index}>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">{res.timestamp ? res.timestamp : "-"}</td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">{res.country ? res.country : "-"}</td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">{res.city ? res.city : "-"}</td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">[{res.location.coordinates[1]}, {res.location.coordinates[0]}]</td>
               </tr>
             ))}
           </tbody>
@@ -37,18 +51,34 @@ export default function CardTable() {
     }
   }
 
+  useEffect(() => {
+    let ignore = false;
+
+    const getAllVisitInfoIds = async () => {
+      axios.defaults.baseURL = "http://localhost:3333";
+      await axios.get(`/api/${urlId}`).then((res) => {
+        const visitInfoIds = res.data.visits;
+        getAllVisitInfo(visitInfoIds);
+      });
+    };
+
+    if (!ignore) {
+      getAllVisitInfoIds();
+    }
+    return () => {
+      ignore = true;
+    };
+  }, [urlId]);
+
   return (
     <>
-      <div
-        className={
-          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-lightBlue-900 text-white"
-        }
-      >
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
         <div className="rounded-t mb-0 px-4 py-3 border-0">
           <div className="flex flex-wrap items-center">
             <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-              <h3 className={"font-semibold text-lg text-blueGray-700"}>
+              <h3 className="font-semibold text-base text-blueGray-700">
                 Link Report
+                <h6>{urlId}</h6>
               </h3>
             </div>
           </div>
@@ -70,21 +100,21 @@ export default function CardTable() {
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Originating Country
+                  Country
                 </th>
                 <th
                   className={
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Originating City
+                  City
                 </th>
                 <th
                   className={
                     "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100"
                   }
                 >
-                  Originating Geolocation Coordinates
+                  Geolocation Coordinates
                 </th>
               </tr>
             </thead>
