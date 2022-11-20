a35b7dad182a8a4216fa0945375e360dcc46b0a1
xinle1030 <xinle1030@gmail.com>
2022-11-20 16:54:57 +0800

    add line chart to dashboard

--- frontend/src/views/admin/Report.js
+++ frontend/src/views/admin/Report.js
@@ -2,19 +2,25 @@ import React from "react";
 
 // components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
+import HeaderRStats from "components/Headers/HeaderRStats.js";
 import CardTable from "components/Cards/CardTable";
+import CardLineChart from "components/Cards/CardLineChart";
 
 export default function Report() {
   return (
     <>
-      <AdminNavbar title="Report"/>
+      <AdminNavbar title="Report" />
       {/* Header */}
-      <HeaderStats />
+      <HeaderRStats />
       <div className="px-4 md:px-10 mx-auto w-full -m-24">
         <div className="flex flex-wrap">
           <div className="w-full px-4">
             <CardTable />
+          </div>
+        </div>
+        <div className="flex flex-wrap mt-4">
+          <div className="w-full px-4">
+            <CardLineChart />
           </div>
         </div>
       </div>
