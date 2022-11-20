3ff639aa92aad896f07c592a50b315197ce27422
xinle1030 <xinle1030@gmail.com>
2022-11-20 16:55:10 +0800

    format linechart and cards

--- frontend/src/views/admin/Dashboard.js
+++ frontend/src/views/admin/Dashboard.js
@@ -2,19 +2,25 @@ import React from "react";
 
 // components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
+import HeaderDStats from "components/Headers/HeaderDStats.js";
 import CardPageVisits from "components/Cards/CardPageVisits.js";
+import CardLineChart from "components/Cards/CardLineChart";
 
 export default function Dashboard() {
   return (
     <>
       <AdminNavbar title="Link Dashboard"/>
       {/* Header */}
-      <HeaderStats />
+      <HeaderDStats />
       <div className="px-4 md:px-10 mx-auto w-full -m-24">
         <div className="flex flex-wrap mt-4">
           <div className="w-full mb-12 xl:mb-0 px-4">
             <CardPageVisits />
+          </div>
+        </div>
+        <div className="flex flex-wrap mt-4">
+          <div className="w-full px-4">
+            <CardLineChart />
           </div>
         </div>
       </div>
