0a543dbf81908e835fe2d3869c1a45b30228368e
xinle1030 <xinle1030@gmail.com>
2022-11-20 14:18:49 +0800

    push VisitInfo to visits array in url for each visit

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import Url from '../models/Url.js';
+import VisitInfo from "../models/VisitInfo.js";
+import { getUserLocation } from "../utils/geo_utils.js";
 
 const router = express.Router();
 
@@ -7,12 +9,26 @@ router.get('/:urlId', async (req, res) => {
   try {
     const url = await Url.findOne({ urlId: req.params.urlId });
     if (url) {
+
+      let visitInfo = new VisitInfo({
+        urlId: url.ObjectID,
+        location: { type: "Point", coordinates: [ 27, 67 ] },
+        timestamp: new Date(),
+      });
+
+      await visitInfo.save();
+
       await Url.updateOne(
         {
           urlId: req.params.urlId,
         },
-        { $inc: { clicks: 1 } }
+        { $inc: { clicks: 1 }, $push: {visits: visitInfo}}
       );
+
+      console.log("check here");
+      const checkUrl = await Url.findOne({ urlId: req.params.urlId });
+      console.log(checkUrl);
+
       return res.redirect(url.origUrl);
     } else res.status(404).json('Not found');
   } catch (err) {
