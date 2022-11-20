e4d92bf6a26968c4f5b9801f033c8cbd313a01c7
xinle1030 <xinle1030@gmail.com>
2022-11-20 14:16:36 +0800

    reformat

--- frontend/src/layouts/Admin.js
+++ frontend/src/layouts/Admin.js
@@ -22,7 +22,7 @@ export default function Admin() {
         <HeaderStats />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
-          <Route path="/home" exact component={CreateForm} />
+            <Route path="/home" exact component={CreateForm} />
             <Route path="/dashboard" exact component={Dashboard} />
             <Redirect from="/" to="/home" />
           </Switch>
