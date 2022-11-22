8cecf07116fdef8a2683a5ef1c546f02bb12e142
xinle1030 <xinle1030@gmail.com>
2022-11-22 15:22:04 +0800

    update route to proviode urls summary

--- backend/routes/urls.js
+++ backend/routes/urls.js
@@ -8,7 +8,8 @@ const router = express.Router();
 // Get all paths
 router.get("/all", async (req, res, next) => {
   console.log("Get all urls");
-  await Url.find({}).sort({_id:-1}) 
+  await Url.find({})
+    .sort({ _id: -1 })
     .then((data) => {
       console.log("Data: ", data);
       res.json(data);
@@ -16,6 +17,33 @@ router.get("/all", async (req, res, next) => {
     .catch((error) => {
       console.log("error: ", error);
     });
+});
+
+router.get("/all/summary", async (req, res, next) => {
+
+  let retData = {
+    totalClicks: 0,
+    topLink: "",
+    topLinkClicks: 0
+  };
+
+  // find totalClicks
+  await Url.aggregate([
+    { $group: { _id: null, totalClicks: { $sum: "$clicks" } } },
+  ]).then((data) => {
+    console.log(data);
+    retData.totalClicks = data[0].totalClicks;
+  });
+
+  // find topUrl
+  let topUrl = await Url.find({}, { shortUrl: 1, clicks: 1 }).sort({ clicks: -1 }).limit(1);
+
+  if (topUrl.length > 0) {
+    retData.topLink = topUrl[0].shortUrl;
+    retData.topLinkClicks = topUrl[0].clicks;
+  }
+
+  return res.json(retData);
 });
 
 router.get("/:urlId", async (req, res) => {
