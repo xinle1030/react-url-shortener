46fe6d03e7e903202981d1e3f2fe84dbd7cfe8a6
xinle1030 <xinle1030@gmail.com>
2022-11-22 17:29:50 +0800

    move API caller to parent

--- frontend/src/views/admin/Report.js
+++ frontend/src/views/admin/Report.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 // components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
@@ -7,15 +9,78 @@ import CardTable from "components/Cards/CardTable";
 import CardLineChart from "components/Cards/CardLineChart";
 
 export default function Report() {
+  const [linkTable, setLinkTable] = useState("");
+  const [clicks, setClicks] = useState(0);
+  const [topCountry, setTopCountry] = useState("");
+
+  const location = useLocation();
+  const urlId = location.state.urlId;
+
+  const getAllMetric = async (metricIds) => {
+    axios.defaults.baseURL = process.env.REACT_APP_BASE;
+
+    await axios
+      .get(
+        `/api/metrics?${metricIds
+          .map((n, index) => `metricIds[${index}]=${n}`)
+          .join("&")}`
+      )
+      .then((res) => {
+        setLinkTable(JSON.stringify(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getMetricSummary = async (metricIds) => {
+    axios.defaults.baseURL = process.env.REACT_APP_BASE;
+
+    await axios
+      .get(
+        `/api/metrics/summary?${metricIds
+          .map((n, index) => `metricIds[${index}]=${n}`)
+          .join("&")}`
+      )
+      .then((res) => {
+        setTopCountry(res.data.topCountry);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    let ignore = false;
+
+    const getAllMetricIds = async () => {
+      axios.defaults.baseURL = process.env.REACT_APP_BASE;
+
+      await axios.get(`/api/urls/${urlId}`).then((res) => {
+        setClicks(res.data.clicks);
+        const metricIds = res.data.metrics;
+        getAllMetric(metricIds);
+        getMetricSummary(metricIds);
+      });
+    };
+
+    if (!ignore) {
+      getAllMetricIds();
+    }
+    return () => {
+      ignore = true;
+    };
+  }, [urlId]);
+
   return (
     <>
       <AdminNavbar title="Report" />
       {/* Header */}
-      <HeaderRStats />
+      <HeaderRStats clicks={clicks} topCountry={topCountry}/>
       <div className="px-4 md:px-10 mx-auto w-full -m-24">
         <div className="flex flex-wrap">
           <div className="w-full px-4">
-            <CardTable />
+            <CardTable linkTable={linkTable}/>
           </div>
         </div>
         <div className="flex flex-wrap mt-4">
