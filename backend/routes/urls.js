889d096bcc46cc9828bdf2cbe567d580d2209449
xinle1030 <xinle1030@gmail.com>
2022-11-24 01:51:00 +0800

    update comment documentation for route in urls

--- backend/routes/urls.js
+++ backend/routes/urls.js
@@ -6,20 +6,7 @@ import {getAllMetricsSummary, getMetricsSummaryByIds} from "./metrics.js";
 
 const router = express.Router();
 
-// Get all paths
-router.get("/all", async (req, res, next) => {
-  console.log("Get all urls");
-  await Url.find({})
-    .sort({ _id: -1 })
-    .then((data) => {
-      console.log("Data: ", data);
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log("error: ", error);
-    });
-});
-
+// Get all link summary
 router.get("/all/summary", async (req, res, next) => {
   console.log("Get all url summary");
   let retData = {
@@ -57,23 +44,21 @@ router.get("/all/summary", async (req, res, next) => {
   return res.json(retData);
 });
 
-router.get("/:urlId", async (req, res) => {
-  console.log("Get certain url");
-  try {
-    const url = await Url.findOne({ urlId: req.params.urlId });
-    console.log(url);
-    console.log(req.params.urlId);
-    if (url) {
-      return res.json(url);
-    } else {
-      res.status(404).json("Not found");
-    }
-  } catch (e) {
-    console.log(err);
-    res.status(500).json("Server Error");
-  }
+// Get all links
+router.get("/all", async (req, res, next) => {
+  console.log("Get all urls");
+  await Url.find({})
+    .sort({ _id: -1 })
+    .then((data) => {
+      console.log("Data: ", data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+    });
 });
 
+// Get summary for a link
 router.get("/:urlId/summary", async (req, res) => {
   console.log("Get certain url summary");
 
@@ -110,6 +95,22 @@ router.get("/:urlId/summary", async (req, res) => {
   return res.json(retData);
 });
 
-
+// Get information for a link
+router.get("/:urlId", async (req, res) => {
+  console.log("Get certain url");
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+    console.log(url);
+    console.log(req.params.urlId);
+    if (url) {
+      return res.json(url);
+    } else {
+      res.status(404).json("Not found");
+    }
+  } catch (e) {
+    console.log(err);
+    res.status(500).json("Server Error");
+  }
+});
 
 export default router;
