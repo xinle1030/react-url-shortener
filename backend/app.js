9d832a5293feb39fa240b5426a39c7ac6ef6f197
xinle1030 <xinle1030@gmail.com>
2022-11-25 00:38:19 +0800

    listen to port when not in testing env

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-dotenv.config({ path: './config/.env' });
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/.env" });
 
 const app = express();
 
@@ -11,21 +12,24 @@ app.use(cors());
 
 connectDB();
 
-import indexRouter from './routes/index.js';
-import urlsRouter from './routes/urls.js';
-import shortUrlsRouter from './routes/short_urls.js';
-import metricsRouter from './routes/metrics.js';
+import indexRouter from "./routes/index.js";
+import urlsRouter from "./routes/urls.js";
+import shortUrlsRouter from "./routes/short_urls.js";
+import metricsRouter from "./routes/metrics.js";
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api', shortUrlsRouter);
-app.use('/api/urls', urlsRouter);
-app.use('/api/metrics', metricsRouter);
-app.use('/', indexRouter);
+app.use("/api", shortUrlsRouter);
+app.use("/api/urls", urlsRouter);
+app.use("/api/metrics", metricsRouter);
+app.use("/", indexRouter);
 
 // Server Setup
 const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
-  console.log(`Server is running at PORT ${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
+  });
+}
+export default app;
