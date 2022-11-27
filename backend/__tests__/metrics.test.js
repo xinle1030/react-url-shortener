bde6203ec6d1c4b3deabd99c466d96392d10e029
xinle1030 <xinle1030@gmail.com>
2022-11-27 09:15:33 +0800

    complete test for Get Metric Summary by Ids

--- backend/__tests__/metrics.test.js
+++ backend/__tests__/metrics.test.js
@@ -1,8 +1,15 @@
 import app from "../app.js";
 import supertest from "supertest"; // supertest is a framework that allows to easily test web apis
-import Metric from "../models/Metric.js";
 import { connect, close, clear } from "../config/test_db.js";
-import { seedUrl, seedMetric } from "../test-setup/seed.js";
+import {
+  seedUrl,
+  seedMetric,
+  expectedMetricSummary,
+} from "../test-setup/seed.js";
+import {
+  getMetricsSummaryByIds,
+  getAllMetricsSummary,
+} from "../routes/metrics.js";
 
 const agent = supertest.agent(app);
 let baseUrl = "/api/metrics/";
@@ -19,7 +26,6 @@ test("GET /all", async () => {
     .get(baseUrl + "/all")
     .expect(200)
     .then((response) => {
-
       expect(response.body.length).toBe(seededMetrics.length);
 
       // expect(response.body[0]).toHaveProperty("country");
@@ -31,34 +37,49 @@ test("GET /all", async () => {
       for (let i in seededMetrics) {
         expect(response.body[i].country).toBe(seededMetrics[i].country);
         expect(response.body[i].city).toBe(seededMetrics[i].city);
-        expect(response.body[i].location).toStrictEqual(seededMetrics[i].location);
+        expect(response.body[i].location).toStrictEqual(
+          seededMetrics[i].location
+        );
         expect(response.body[i].timestamp).toEqual(expect.anything());
       }
     });
 });
-
 
 test("GET /", async () => {
   let seededUrl = await seedUrl();
   let seededMetrics = await seedMetric(seededUrl);
 
-  let metrisIds = seededMetrics.map(metric => metric.id);
+  let metricIds = seededMetrics.map((metric) => metric.id);
 
   await agent
     .get(baseUrl)
-    .query({metricIds: metrisIds})
+    .query({ metricIds: metricIds })
     .expect(200)
     .then((response) => {
-
-      expect(response.body.length).toBe(metrisIds.length);
+      expect(response.body.length).toBe(metricIds.length);
 
       // Check data
       for (let i in seededMetrics) {
         expect(response.body[i].country).toBe(seededMetrics[i].country);
         expect(response.body[i].city).toBe(seededMetrics[i].city);
-        expect(response.body[i].location).toStrictEqual(seededMetrics[i].location);
+        expect(response.body[i].location).toStrictEqual(
+          seededMetrics[i].location
+        );
         expect(response.body[i].timestamp).toEqual(expect.anything());
       }
     });
 });
 
+test("Get Metric Summary by Ids", async () => {
+  let seededUrl = await seedUrl();
+  let seededMetrics = await seedMetric(seededUrl);
+
+  let metricIds = seededMetrics.map((metric) => metric.id);
+
+  let metricsSummary = await getMetricsSummaryByIds(metricIds);
+
+  console.log(metricsSummary);
+  console.log(expectedMetricSummary);
+
+  expect(metricsSummary).toStrictEqual(expectedMetricSummary);
+});
