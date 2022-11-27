9fef1a11987463893b29916e38e388e69974311a
xinle1030 <xinle1030@gmail.com>
2022-11-27 21:41:58 +0800

    format doc

--- backend/__tests__/index.test.js
+++ backend/__tests__/index.test.js
@@ -1,9 +1,7 @@
 import app from "../app.js";
 import supertest from "supertest"; // supertest is a framework that allows to easily test web apis
 import { connect, close, clear } from "../config/test_db.js";
-import {
-  seedUrl,
-} from "../test-setup/seed.js";
+import { seedUrl } from "../test-setup/seed.js";
 
 const agent = supertest.agent(app);
 
@@ -12,8 +10,9 @@ afterEach(async () => await clear());
 afterAll(async () => await close());
 
 test("GET /:urlId", async () => {
-
   let seededUrl = await seedUrl();
 
-  await agent.get("/" + seededUrl.urlId).expect(302);
+  await agent
+    .get("/" + seededUrl.urlId)
+    .expect(302);
 });
