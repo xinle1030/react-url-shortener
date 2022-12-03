602b21f772433d7926af1094a0c3175057fd2865
LAMXINLE <xlam0003@student.monash.edu>
2022-12-03 17:54:08 +0800

    specify swaggerDefinition

---
+++ backend/swagger-config/swagger.js
@@ -0,0 +1,21 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerDefinition = {
+  info: {
+    title: 'REST API for url-shortener', // Title of the documentation
+    version: '1.0.0', // Version of the app
+    description: 'This is the REST API for my url-shortener', // short description of the app
+  },
+  host: 'localhost:3333', // the host or url of the app
+  basePath: '/', // the basepath of your endpoint
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./swagger-docs/**/*.yaml'],
+};
+// initialize swagger-jsdoc
+export default swaggerJSDoc(options);
