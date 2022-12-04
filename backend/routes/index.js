e51cc5349a0b308a678032b1e0f5073b19c6b5b4
LAMXINLE <xlam0003@student.monash.edu>
2022-12-04 17:14:41 +0800

    check for client ip address only if not on localhost

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import axios from 'axios';
 import Url from "../models/Url.js";
 import Metric from "../models/Metric.js";
+import IP from 'ip';
 
 const router = express.Router();
 
@@ -18,9 +19,16 @@ router.get("/:urlId", async (req, res) => {
 
   try {
     const url = await Url.findOne({ urlId: req.params.urlId });
-    let ipAddress = req.ip;
 
-    await axios.get(`https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.ABSTRACT_IP_GEO_API_KEY}&ip_address=${ipAddress}`)
+    let ipAddress = req.ip;
+    let requestUrl = `https://ipgeolocation.abstractapi.com/v1/?api_key=${process.env.ABSTRACT_IP_GEO_API_KEY}`;
+
+    // if not on localhost, check for client ip address
+    if (ipAddress != "::1"){
+      requestUrl += `&ip_address=${ipAddress}`;
+    }
+
+    await axios.get(requestUrl)
     .then(response => {
         console.log(response.data);
         locObj = {
