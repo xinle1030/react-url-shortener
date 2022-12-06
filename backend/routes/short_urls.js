a8320aa731cdd230a6c7c0e8e3b16f7be566ef45
LAMXINLE <xlam0003@student.monash.edu>
2022-12-06 17:22:01 +0800

    extract title metadata from url

--- backend/routes/short_urls.js
+++ backend/routes/short_urls.js
@@ -2,20 +2,38 @@ import express from "express";
 import { nanoid } from "nanoid";
 import Url from "../models/Url.js";
 import { validateUrl } from "../utils/utils.js";
+import fetch from "node-fetch";
 import dotenv from "dotenv";
 dotenv.config({ path: "../config/.env" });
 
 const router = express.Router();
 
+const parseTitle = (body) => {
+  let match = body.match(/<title>([^<]*)<\/title>/) // regular expression to parse contents of the <title> tag
+  if (!match || typeof match[1] !== 'string')
+    throw new Error('Unable to parse the title tag')
+  return match[1]
+}
+
 // Short URL Generator
 router.post("/short", async (req, res) => {
   console.log("Shorten URL");
-  const { origUrl, title } = req.body;
+  const { origUrl } = req.body;
+  let title;
   const base = process.env.BASE;
   // const base = "https://url-shortener-slink.herokuapp.com";
 
   const urlId = nanoid(15);
   if (validateUrl(origUrl)) {
+
+    await fetch(origUrl)
+    .then(res => res.text()) // parse response's body as text
+    .then(body => parseTitle(body)) // extract <title> from body
+    .then(retTitle => title = retTitle) // send the result back
+    .catch(e => res.status(500).end(e.message)) // catch possible errors
+
+    console.log(title);
+
     try {
       const shortUrl = `${base}/${urlId}`;
 
